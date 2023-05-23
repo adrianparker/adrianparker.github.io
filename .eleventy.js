a976b06dc74bb884eac75f18279eced4b877b9b8
Adrian Parker <adrian@iceknife.com>
2023-05-23 23:45:11 +1200

    Clean up hack v1, remove commented code; etc

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,22 @@
+const markdownIt = require("markdown-it");
+
 module.exports = function (eleventyConfig) {
+
+    // output everything from the static folder at root of output
     eleventyConfig.addPassthroughCopy({ static: "/" });
+
+    // can use the shortcode 'md' in a page to render markdown copy as HTML in place
+    eleventyConfig.addFilter("md", function (content = "") {
+      return markdownIt({ html: true }).render(content);
+    });
+
+    // makes the content before the <!-- excerpt --> of each post available
     eleventyConfig.setFrontMatterParsingOptions({
       excerpt: true,
       excerpt_separator: "<!-- excerpt -->"
     });
-    eleventyConfig.addCollection("popularPosts", function(collectionApi) {
-      return collectionApi.getFilteredByTag("popular");
-    });
+
+    // add up to 3 most recent posts collection to global scope for base template to use on each page nav
     eleventyConfig.addCollection("recentPosts", function(collectionApi) {
       var recentPosts= collectionApi.getFilteredByTag("popular").reverse();
       if (recentPosts.length > 3){
